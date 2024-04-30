1eda4313c39538d2f68fd5c1d8a16173b0dbc4be
AleksandraSkrypnik <165397784+AleksandraSkrypnik@users.noreply.github.com>
2024-04-30 20:48:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         int weightKilogram = 98;
         double heightMeter = 1.87;
-        int bodyMassIndex = service.calculate(98, 1.87); // должно получиться 500
+        int bodyMassIndex = service.calculate(weightKilogram, heightMeter); // должно получиться 500
         System.out.println(bodyMassIndex);
     }
 }
